8571487f6136ef4255d40db0f3ede4109e156145
Татьяна Нефедова <tatka906090isakova@yandex.ru>
2023-05-23 16:36:23 +0300

    Sprint7

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+
+from .models import Birthday, Tag
+
+
+class BirthdayAdmin(admin.ModelAdmin):
+    model = Birthday
+
+
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+
+
+admin.site.register(Birthday, BirthdayAdmin)
+admin.site.register(Tag, TagAdmin)
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,35 +1,80 @@
-from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
+from django.urls import reverse_lazy
 
-from .forms import BirthdayForm
-from .utils import calculate_birthday_countdown
+from .forms import BirthdayForm, CongratulationForm
 from .models import Birthday
+from .utils import calculate_birthday_countdown
 
 
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-    form = BirthdayForm(request.POST or None, instance=instance)
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
+class BirthdayListView(ListView):
+    model = Birthday
+    queryset = Birthday.objects.prefetch_related(
+        'tags'
+    ).select_related('author')
+    ordering = 'id'
+    paginate_by = 10
+
+
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class BirthdayUpdateView(LoginRequiredMixin, UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+    def dispatch(self, request, *args, **kwargs):
+        get_object_or_404(Birthday, pk=kwargs['pk'], author=request.user)
+        return super().dispatch(request, *args, **kwargs)
+
+
+class BirthdayDeleteView(LoginRequiredMixin, DeleteView):
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+    def dispatch(self, request, *args, **kwargs):
+        get_object_or_404(Birthday, pk=kwargs['pk'], author=request.user)
+        return super().dispatch(request, *args, **kwargs)
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
         )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context) 
+        context['form'] = CongratulationForm()
+        context['congratulations'] = (
+            self.object.congratulations.select_related('author')
+        )
+        return context
 
-def birthday_list(request):
-    birthdays = Birthday.objects.all()
-    context = {'birthdays': birthdays}
-    return render(request, 'birthday/birthday_list.html', context)
 
-def delete_birthday(request, pk):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context)
+@login_required
+def simple_view(request):
+    return HttpResponse('Страница для залогиненных пользователей!')
+
+
+@login_required
+def add_comment(request, pk):
+    birthday = get_object_or_404(Birthday, pk=pk)
+    form = CongratulationForm(request.POST)
+    if form.is_valid():
+        congratulation = form.save(commit=False)
+        congratulation.author = request.user
+        congratulation.birthday = birthday
+        congratulation.save()
+    return redirect('birthday:detail', pk=pk)
